a596715c516fec2dc049915349c7dfc5cfd83778
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-01-07 23:10:03 +0000

    lavoro finito

--- Wijeratne/src/app/app.component.ts
+++ Wijeratne/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { QuartoComponent } from './quarto/quarto.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SecondoComponent, TerzoComponent, QuartoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Wijeratne';
+  title = 'app';
 }
--- Wijeratne/src/app/secondo/secondo.component.ts
+++ Wijeratne/src/app/secondo/secondo.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './secondo.component.css'
 })
 export class SecondoComponent {
-
+  title = 'secondo';
 }
